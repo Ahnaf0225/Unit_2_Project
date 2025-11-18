1f6c2ec1da7a8c144e9938db3cd0242c117ebaf6
MdHabib <mdh88@nycstudents.net>
2025-11-18 08:40:46 -0500

    update

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -1,8 +1,10 @@
 public class MemoryGame {
-    private String color;
+
     private int number;
 
-    public MemoryGame (String blue, String yellow, String red, String green){
+    public MemoryGame (Integer RandomNum){
+
+        number = RandomNum;
 
 
 
